6d1e197a3f8807b8d7f4fb61e0a3dacfc5ebc38a
Andrew Burnett <andrew@andburn.info>
2018-03-23 21:46:13 +0000

    Replace Jekyll with simple repo list

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
----
-layout: null
----
-$(document).ready(function () {
-  $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+$(function() {
+
+    function createListItem(key, obj) {
+        return "<li id='" + key + "'>" + 
+            "<a href='" + obj.html_url + "'>" + obj.name + "</a>" + 
+            "<span>" + obj.description + "</span></li>";
+
     }
-  })
 
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
-
-})
+    $.getJSON("https://api.github.com/users/andburn/repos")
+        .done(function(data) {
+            var items = [];
+            $.each( data, function( key, val ) {
+              items.push(createListItem(key, val));
+            });
+           
+            $("<ul/>", {
+              "id": "repo-list",
+              html: items.join("")
+            }).appendTo("#container");
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            $("<p class='error'>" + error + "</p>")
+                .appendTo("#container");
+        });
+});
